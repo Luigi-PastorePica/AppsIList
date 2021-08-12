21326bcb4fe76ef85aa1be9a2e5d097a9efc71f5
Luigi-PastorePica <luigi.pastorepica@outlook.com>
2021-08-12 15:38:38 -0400

    Added initialization for resource classes

--- detailedresource.py
+++ detailedresource.py
@@ -3,6 +3,22 @@ from resource import Resource
 
 class DetailedResource(Resource):
 
+    def __init__(self, detailed_resource_data):
+        # TODO Might not need the two dictionaries, I can just pass a single dict with as much or little info as needed
+        resource_data: dict = {
+            "rid": detailed_resource_data["rid"],
+            "title": detailed_resource_data["title"],
+            "image_link": detailed_resource_data["image_link"],
+            "category": detailed_resource_data["category"],
+            "rating": detailed_resource_data["rating"]
+        }
+        super(DetailedResource, self).__init__(resource_data)
+
+        self.length: str = detailed_resource_data["length"]
+        self.description: str = detailed_resource_data["description"]
+        self.origin: str = detailed_resource_data["origin"]
+        self.reviews: str = detailed_resource_data["reviews"]
+
     def set_length(self):
         pass
 
--- resource.py
+++ resource.py
@@ -1,6 +1,18 @@
 class Resource:
     def __init__(self):
-        pass
+        self.rid: int
+        self.title: str
+        self.image_link: str
+        self.category: str
+        self.rating: float
+
+    # def __init__(self, rid: int, title: str, image_link: str, category: str, rating: float):
+    def __init__(self, resource_data: dict):
+        self.rid: int = resource_data["rid"]
+        self.title: str = resource_data["title"]
+        self.image_link: str = resource_data["image_link"]
+        self.category: str = resource_data["category"]
+        self.rating: float = resource_data["rating"]
 
     def set_id(self):
         pass
@@ -8,7 +20,7 @@ class Resource:
     def set_title(self):
         pass
 
-    def set_image(self):
+    def set_image_link(self):
         pass
 
     def set_category(self):
@@ -23,7 +35,7 @@ class Resource:
     def get_title(self):
         pass
 
-    def get_image(self):
+    def get_image_link(self):
         pass
 
     def get_category(self):
