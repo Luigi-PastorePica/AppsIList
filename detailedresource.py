37e6e94ee504ca7d24f94c02fcccf49f5c82eb68
Luigi-PastorePica <luigi.pastorepica@outlook.com>
2021-08-12 00:31:47 -0400

    Added skeleton for resource, detailedresource

---
+++ detailedresource.py
@@ -0,0 +1,28 @@
+from resource import Resource
+
+
+class DetailedResource(Resource):
+
+    def set_length(self):
+        pass
+
+    def set_description(self):
+        pass
+
+    def set_origin(self):
+        pass
+
+    def set_reviews(self):
+        pass
+
+    def get_length(self):
+        pass
+
+    def get_description(self):
+        pass
+
+    def get_origin(self):
+        pass
+
+    def get_reviews(self):
+        pass
---
+++ resource.py
@@ -0,0 +1,33 @@
+class Resource:
+    def __init__(self):
+        pass
+
+    def set_id(self):
+        pass
+
+    def set_title(self):
+        pass
+
+    def set_image(self):
+        pass
+
+    def set_category(self):
+        pass
+
+    def set_rating(self):
+        pass
+
+    def get_id(self):
+        pass
+
+    def get_title(self):
+        pass
+
+    def get_image(self):
+        pass
+
+    def get_category(self):
+        pass
+
+    def get_rating(self):
+        pass
