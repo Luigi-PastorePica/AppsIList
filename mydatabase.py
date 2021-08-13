9c7729e139f87fed344617fb697ecf6119eed718
Luigi-PastorePica <luigi.pastorepica@outlook.com>
2021-08-12 21:09:34 -0400

    Extracted add_item functionality
    Moved functionality to add new items to the database from server.py's add_item() to MyDatabase.add_resource and Mydatabase.format_new_resource()

--- mydatabase.py
+++ mydatabase.py
@@ -2,8 +2,8 @@ from csv import DictReader, DictWriter
 from json import dumps as dumps
 from copy import deepcopy as deepcopy
 
-# db = []
-current_id = 12  # TODO get from last item in csv file
+from resource import Resource as Resource
+
 # TODO need to add a "resource_type" field (e.g. book, blogpost, video, course, etc.). This would work for the basic view.
 fieldnames = ["id", "title", "media", "description", "numerical", "external_link", "list"]  # TODO Handle differently
 basic_fieldnames = ["id", "title", "media", "numerical"]  # TODO Handle differently
@@ -14,20 +14,18 @@ class MyDatabase:
     def __init__(self, file_name: str):
         self.db_name = file_name
         self.db: [dict] = []  # TODO this should change to be a list of Resource objects.
+        self.current_id = self.get_newest_id()
 
-    # TODO Load only basic info.
-    # TODO Too many levels of abstraction. Might need to extract functionality.
-    # Load entries in "database" into memory
     def load_basic_data(self) -> list:
+        '''Loads basic data from entries in the "database" into memory'''
+        # TODO Too many levels of abstraction. Might need to extract functionality.
         resource_basic: dict = {}
         with open(self.db_name, "r") as csvfile:
             reader = DictReader(csvfile)
             for row in reader:
-                # row["list"] = eval(row["list"])  # String to Dict for reviews # Not needed for basic data
                 for field in basic_fieldnames:
                     resource_basic[field] = row[field]
                 self.db.append(deepcopy(resource_basic))
-
         return self.db
 
     # TODO consider case where rid is not found. This should not happen, but still, it is good to have a backup
@@ -46,8 +44,8 @@ class MyDatabase:
         return resource_data
 
     # TODO Deprecate..... maybe
-    # Load entries in "database" into memory
-    def load_data(self) -> list:
+    def load_data(self) -> [dict]:
+        '''Load entries in "database" into memory'''
         with open(self.db_name, "r") as csvfile:
             reader = DictReader(csvfile)
             for row in reader:
@@ -55,3 +53,76 @@ class MyDatabase:
                 self.db.append(row)
 
         return self.db
+
+    def get_newest_id(self) -> int:
+        # TODO This is a hack, find if there is a more efficient way of doing this
+        newest_id = 0
+        with open(self.db_name, "r") as csvfile:
+            reader = DictReader(csvfile)
+            for row in reader:
+                newest_id = int(row["id"])
+
+        return newest_id
+
+    def add_resource(self, resource_data: dict) -> int:
+
+        item_row = self.format_new_resource(resource_data)
+        # print("resource_data (previously json_data):\n{}".format(resource_data))  # Debugging
+
+        # Write new item to database
+        try:
+            with open(self.db_name, "a") as csvfile:
+                writer = DictWriter(csvfile, fieldnames=fieldnames)
+                writer.writerow(item_row)
+        except FileNotFoundError as e:
+            print(e, "Creating new file")
+            with open("data.csv", "w") as output_file:
+                writer = DictWriter(output_file, fieldnames=fieldnames)
+                writer.writeheader()
+                writer.writerow(item_row)
+        except Exception as e:
+            # TODO return something that tells the user the record could not be created
+            raise e
+
+        newest_id = int(item_row["id"])
+
+        if newest_id == self.get_newest_id():
+            self.current_id = self.get_newest_id()
+        else:
+            # TODO Find a way to notify that something went wrong
+            pass
+
+        return newest_id
+
+    def format_new_resource(self, resource_data: dict) -> dict:
+        '''
+        Creates a new row/entry for the database in a dictionary format
+
+            Parameters:
+                resource_data(dict): Contains the data of the record to be added to the database.
+
+            Returns:
+                item_row(dict): Contains properly formatted data for the new record to be added to the database
+        '''
+
+        newest_id = self.current_id + 1
+        item_row: dict = {}
+
+        # Creating row to be stored
+        for field_name in fieldnames:
+            if field_name == "id":
+                item_row[field_name] = newest_id
+            elif field_name == "list":
+                # print("type of json_data[\"list_elem\"]:\n{}".format(type(resource_data["list_elem"])))  # Debugging
+                # print("json_data[\"list_elem\"]:\n{}".format(resource_data["list_elem"]))  # Debugging
+                # print("dumps(json_data[\"list_elem\"]:\n{}".format(dumps(resource_data["list_elem"])))  # Debugging
+                user_id = resource_data["list_elem"].keys();
+                # Using string instead of boolean simplifies storage and retrieval from a csv file
+                resource_data["list_elem"][list(user_id)[0]]["mark_as_deleted"] = "False"
+                item_row[field_name] = dumps(resource_data["list_elem"])  # Dict to String (for review entries)
+            elif field_name is None or resource_data[field_name] is None:
+                item_row[field_name] = None
+            else:
+                item_row[field_name] = resource_data[field_name]
+
+        return item_row
--- server.py
+++ server.py
@@ -7,7 +7,7 @@ from copy import deepcopy as deepcopy
 
 from mydatabase import MyDatabase as MyDatabase
 
-current_id = 12  # TODO get from last item in csv file
+# current_id = 12  # TODO get from last item in csv file
 fieldnames = ["id", "title", "media", "description", "numerical", "external_link", "list"]
 
 NEWEST_ITEMS = 10
@@ -16,7 +16,8 @@ app = Flask(__name__)
 
 db_file_path = "data.csv"
 mdb = MyDatabase(db_file_path)
-db: [list] = mdb.load_basic_data()
+db: [dict] = mdb.load_basic_data()
+current_id = mdb.get_newest_id()
 
 
 @app.route('/')
@@ -33,14 +34,15 @@ def go_to_home():
     # print(dumps(newest, indent=2))  # Debugging
     return render_template("home.html", newest=newest)
 
+
 @app.route('/view/<id_str>')
 def go_to_view(id_str=None):
     print("VIEW")
     global db
     id_nbr = int(id_str)
-    details = {}  # TODO this should use id_str to pull resource details from database and place them in DetailedResource obj.
+    details: dict = {}  # TODO this should use id_str to pull resource details from database and place them in DetailedResource obj.
 
-    # This is how detailed info used to be obtained; i.e. from the already loaded in-memory db.
+    # TODO Remove block: This is how detailed info used to be obtained; i.e. from the already loaded in-memory db.
     '''
     for item in db:  # TODO Use hashmap or another search method instead for efficiency. Note: can be fixed by using RDB
         if int(item["id"]) == id_nbr:
@@ -53,7 +55,7 @@ def go_to_view(id_str=None):
     details = mdb.load_detailed_resource(id_nbr)
     print("item with id {}: {}".format(id_nbr, details))
 
-    # TODO Last time I left here
+    # TODO Last time I left here. This is from back when I first did this project. No idea what this is about.
     # for review in details["list"]:
 
     # print(type(details["list"]))  # Debugging
@@ -72,70 +74,15 @@ def go_to_create_item():
 @app.route('/add_item', methods=['POST'])
 def add_item():
     print("ADD ITEM")
-    global current_id
-    global db
-    item_row = {}
 
     json_data = request.get_json()
-    # print(json_data)  # Debugging
-    current_id += 1
+    # print("json_data: {}".format(json_data))  # Debugging
 
-    # Creating row to be stored
-    for field_name in fieldnames:
-        # print(field_name)
-        if field_name == "id":
-            # print("I am inside the id conditional statement")  # Debugging
-            item_row[field_name] = current_id
-        elif field_name == "list":
-            # print("I am inside the list conditional statement")  # Debugging
-            print(type(json_data["list_elem"]))
-            print(json_data["list_elem"])
-            print(dumps(json_data["list_elem"]))
-            user_id = json_data["list_elem"].keys();
-            # Using string instead of boolean simplifies storage and retrieval from a csv file
-            json_data["list_elem"][list(user_id)[0]]["mark_as_deleted"] = "False"
-            item_row[field_name] = dumps(json_data["list_elem"])  # Dict to String
-        elif field_name is None or json_data[field_name] is None:
-            item_row[field_name] = None
-        else:
-            # print("I am inside the else statement")  # Debugging
-            item_row[field_name] = json_data[field_name]
+    new_id = mdb.add_resource(json_data)
 
-    try:
-        with open("data.csv", "a") as csvfile:
-            writer = DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writerow(item_row)
-    except FileNotFoundError as e:
-        print(e, "Creating new file")
-        with open("data.csv", "w") as output_file:
-            writer = DictWriter(output_file, fieldnames=fieldnames)
-            writer.writeheader()
-            writer.writerow(item_row)
-    except Exception as e:
-        current_id -= 1
-        # TODO return something that tells the user the record could not be created
-        raise e
-    print("APPEND TO DB")
-    # db.append(item_row) # NOOO this is the thing that is causing inconsistencies.
-    print(type(item_row["list"]))
-    print(item_row["list"])
-    item_row["list"] = eval(item_row["list"])
-    db.append(item_row)
-    print(type(item_row["list"]))
-    print(item_row["list"])
+    # Provides the link to the new page so the client can be redirected there
+    new_link = "view/" + str(new_id)
 
-    # try:
-    #     print(type(item_row["list"]))
-    #     print(item_row["list"])
-    #     item_row["list"] = eval(item_row["list"])
-    #     db.append(item_row)
-    #     print(type(item_row["list"]))
-    #     print(item_row["list"])
-    # except NameError as e:
-    #     if
-    new_link = "view/" + str(current_id)
-    # print(new_link)  # Debugging
-    # send back the link to the new page so the client can be redirected there
     return jsonify(link=new_link)
 
 
